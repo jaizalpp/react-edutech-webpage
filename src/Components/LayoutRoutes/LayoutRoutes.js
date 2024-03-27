3dfad842104ac7792d19c93f5c85b2e514cde053
jaizal puthiyaparambil <jaizaljpkl@gmail.com>
2024-03-27 10:02:29 +0530

    initial commit

--- src/Components/LayoutRoutes/LayoutRoutes.js
+++ src/Components/LayoutRoutes/LayoutRoutes.js
@@ -20,7 +20,7 @@ function LayoutRoutes() {
 
         <Routes>
             <Route path = '/' element={<Home/>} />
-            <Route path = '/edutech-react-website' element={<Home/>} />
+            <Route path = '/online-education-react-web' element={<Home/>} />
             <Route path = '/services' element={<Plans/>} />
             <Route path = '/courses' element={<Courses/>} />
             <Route path = '/about' element={<AboutUs/>} />
