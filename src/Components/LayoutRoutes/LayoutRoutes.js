4bc461a32d90a69199eb27ddffa5474493e09726
jaizalpp <jaizaljpkl@gmail.com>
2024-04-03 02:02:15 +0530

    initial commit

--- src/Components/LayoutRoutes/LayoutRoutes.js
+++ src/Components/LayoutRoutes/LayoutRoutes.js
@@ -22,7 +22,8 @@ function LayoutRoutes() {
 
 
           
-            <Route path = '/home' element={<Home/>} />
+            <Route path='/' element={<Home/>}/>
+            <Route path='/react-edutech-webpage' element={<Home/>}/>
             <Route path = '/services' element={<Plans/>} />
             <Route path = '/courses' element={<Courses/>} />
             <Route path = '/about' element={<AboutUs/>} />
